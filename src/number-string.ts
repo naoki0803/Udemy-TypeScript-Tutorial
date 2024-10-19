2e0f0996152dcf288c39edaa97c5212723bbdb2a
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-10-20 06:50:14 +0900

    Type-nsumber-string

---
+++ src/number-string.ts
@@ -0,0 +1,13 @@
+export {};
+
+let year: number = 1985;
+// number以外は代入不可
+// year = false;
+console.log(year);
+
+let age: number = 0x2b;
+console.log(age);
+
+let name: string = 'Hm';
+// name = 10;
+console.log(name);
