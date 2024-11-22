66bb4807f2afe21386da5ac80dc3c8e07241976b
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-11-23 07:18:41 +0900

    add sumWithInitial

--- src/36.rest-parameters.ts
+++ src/36.rest-parameters.ts
@@ -1,6 +1,12 @@
 export {};
 
-const array: number[] = [1, 2, 3, 4];
+/**
+ * rest-parameterとは
+ *
+ * 関数の引数として不特定の数の引数を受け取ることができる構文です。
+ * これにより、配列として引数を扱うことができます。
+ * 例えば、sum関数は任意の数の数値を受け取り、その合計を計算します。
+ */
 
 //reduce内で利用するコールバック関数
 const reducer = (accumulator: number, currentValue: number): number => {
@@ -9,8 +15,17 @@ const reducer = (accumulator: number, currentValue: number): number => {
 
 //restParameterで受け取ると、数値の配列になる。
 const sum: (...values: number[]) => number = (...values: number[]) => {
-    console.log(values); //[ 1, 2, 3, 4, 5 ]
     return values.reduce(reducer);
 };
 
-console.log(sum(1, 2, 3, 4, 5));
+//option引数のinitialValueを追加した場合、rest公文は引数の最後に移動する必要がある。
+const sumWithInitial: (initialValue: number, ...values: number[]) => number = (
+    initialValue: number,
+    ...values: number[]
+) => {
+    console.log(values); //[ 1, 2, 3, 4, 5 ]
+    return values.reduce(reducer, initialValue);
+};
+
+console.log(sum(5, 1, 2, 3, 4));
+console.log(sumWithInitial(5, 1, 2, 3, 4));
